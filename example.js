0eaa0b8d9888376a738bf301f4ac3f47c181a342
Green_Lab <greencoder001@outlook.com>
2022-04-25 19:43:45 +0200

    Added item3 in example

--- example.js
+++ example.js
@@ -2,6 +2,7 @@ const { SimilarityItem } = require('.') // Require the local module
 
 const item1 = SimilarityItem.create(['geodreieck', 'zeichnen', 'geometrie'], 'Foo', 'Foo description')
 const item2 = SimilarityItem.create(['geobrett', 'geometrische formen', 'mathe', 'geometrie'], 'Bar', 'Foo description')
+const item3 = SimilarityItem.create(['deutsch', 'satzglieder', 'prädikat'], 'Foo', 'Baz description')
 
 const similarity = item1.calculateSimilarity(item2)
 const highestScoringKeyword = item1.getHighestScoringKeyword(item2)
@@ -10,6 +11,8 @@ const averageScore = item1.getAverageScore(item2)
 console.log('Similarity:', similarity)
 console.log('Highest scoring keyword:', highestScoringKeyword)
 console.log('Average Score:', averageScore)
+
+console.log('Similarity to item3:', item1.calculateSimilarity(item3))
 
 // TODO: Compare title & description
 // TODO: Add SimilarityLists
